d67ae325b7ab081c391bd67ea857b9f78633bb81
thesuperfalcon <tintinfalk01@gmail.com>
2024-04-11 12:40:46 +0200

    Test

--- script/script.js
+++ script/script.js
@@ -276,7 +276,7 @@ function updatePicture(playerX, playerY, catPositions, zombiePositions) {
     let pictureIndex = currentPlayerPicture.x * 7 + currentPlayerPicture.y;
     pictureIndex = Math.min(Math.max(pictureIndex, 0), pictures.length - 1);
     let picture = pictures[pictureIndex];
-    pictureElement.style.backgroundImage = `url(../images/${picture}.jpg)`;
+    pictureElement.style.backgroundImage = `url(./images/${picture}.jpg)`;
 }
 
 function norrisApi(query) {
