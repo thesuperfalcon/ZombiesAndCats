c7efeca8c13b87cec8384b3333915c93d07770fe
thesuperfalcon <tintinfalk01@gmail.com>
2024-04-12 08:08:27 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -278,13 +278,15 @@ function updatePicture(playerX, playerY, catPositions, zombiePositions) {
     let picture = pictures[pictureIndex];
     let pictureElement = document.querySelector(".picture");
 
+    pictureElement.src = `images/${picture}.jpg`; 
+
     // Get the base URL of the current script
-    const scriptUrl = document.currentScript.src;
+    /*const scriptUrl = document.currentScript.src;
     const scriptPath = scriptUrl.substring(0, scriptUrl.lastIndexOf("/") + 1);
     const imagePath = scriptPath + `../images/${picture}.jpg`;
 
     // Set the src attribute of the pictureElement
-    pictureElement.src = imagePath;
+    pictureElement.src = imagePath;*/
 }
 function norrisApi(query) {
     const apiUrl = `https://api.chucknorris.io/jokes/search?query=${query}`;
